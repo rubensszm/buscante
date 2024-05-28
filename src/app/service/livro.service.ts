9ad1b3775b43da0667acccc6c95aefb82bf7678a
Rubens Martins <rubens.martins@tst.jus.br>
2024-05-28 14:03:33 -0300

    Does not throws an error when the book query brings zero results, presenting the information to the user

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -11,12 +11,13 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
+    return this.http.get<LivrosResultado>(this.API, { params })
+    // .pipe(
       // tap(retorno => console.log('Fluxo do tap: ', retorno)),
-      map(resultado => resultado.items),
+      // map(resultado => resultado.items ?? []),
       // tap(resultado => console.log('Fluxo depois do map: ', resultado))
-    )
+    // )
   } 
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,11 +1,12 @@
 import { FormControl } from '@angular/forms';
-import { Item } from './../../models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
 import { Component } from '@angular/core';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
-import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import { EMPTY, catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap, throwError } from 'rxjs';
 
 const PAUSA = 500
+
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -14,8 +15,23 @@ const PAUSA = 500
 export class ListaLivrosComponent {
 
   campoBusca = new FormControl
+  mensagemErro = ''
+  livrosResultado: LivrosResultado
 
   constructor(private service: LivroService) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges
+  .pipe(
+    debounceTime(PAUSA),
+    filter(valorDigitado => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo inicial')),
+    switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro)
+      return of()
+    })
+  )
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
@@ -25,7 +41,14 @@ export class ListaLivrosComponent {
       distinctUntilChanged(),
       switchMap(valorDigitado => this.service.buscar(valorDigitado)),
       tap(() => console.log('Requisição ao servidor')),
-      map(items => this.livrosResultadoParaLivros(items))
+      map(resultado => resultado.items ?? []),
+      map(items => this.livrosResultadoParaLivros(items)),
+      catchError(erro => {
+        // this.mensagemErro = 'Ocorreu um erro'
+        // return EMPTY
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro = 'Ocorreu um erro'))
+      })
     )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
